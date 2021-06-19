c9b8bf24b971e405ccc7c6761424d0b822eac37d
vitor.bacelar <v.bacelar@intcom.com.br>
2021-06-19 15:37:17 -0300

    finish challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      MaterialApp(home: BallPage()),
+    );
+
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void rollBall() {
+    ballNumber = Random().nextInt(4) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: TextButton(
+            onPressed: () {
+              setState(() {
+                rollBall();
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png')),
       ),
     );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask me anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
